7dc7371afb2d36007f22adfd259b86075533fc41
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-02-18 19:30:10 +0200

    Added comparing my function with built-in function results

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        int number = 23;
-        int bigEndian = EndiannessConverter.convertEndianness(number);
-        System.out.println("In big endian "+bigEndian);
-        System.out.println("In little endian "+ EndiannessConverter.convertEndianness(bigEndian));
+        Random random = new Random();
+        int number = random.nextInt(Integer.MAX_VALUE - 200)+ 200;
+        System.out.println("Generated number: "+number);
+        int myBigEndian = EndiannessConverter.convertEndianness(number);
+        int integerBigEndian = Integer.reverseBytes(number);
+        System.out.println("In big endian my function: "+myBigEndian+ "\tIn big endian Integer function: "+ integerBigEndian);
+        System.out.println("In little endian my function: "+ EndiannessConverter.convertEndianness(myBigEndian)
+                + "\tIn little endian Integer function: "+Integer.reverseBytes(integerBigEndian));
     }
 }
