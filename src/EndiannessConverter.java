41c110a635023f905ee119a4614389624a2df0ee
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-02-18 15:18:46 +0200

    Added functionality for converting endianness

---
+++ src/EndiannessConverter.java
@@ -0,0 +1,19 @@
+public class EndiannessConverter {
+    public static int convertEndianness(int number){
+        byte[] bytes = new byte[Integer.BYTES];
+        int length = bytes.length;
+        for (int i = 0; i < length; i++) {
+            bytes[i] = (byte) (number & 0xFF);
+            number >>= 8;
+        }
+        return byteArrayToInt(bytes);
+    }
+    private static int byteArrayToInt(byte[] bytes){
+        int value = 0;
+        for (byte b : bytes) {
+            value = (value << 8) + (b & 0xFF);
+        }
+        return value;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        int number = 23;
+        int bigEndian = EndiannessConverter.convertEndianness(number);
+        System.out.println("In big endian "+bigEndian);
+        System.out.println("In little endian "+ EndiannessConverter.convertEndianness(bigEndian));
     }
 }
